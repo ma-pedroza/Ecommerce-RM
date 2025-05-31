9afc149e37827949f6e42d37637e4729801d1956
Roddy192 <149844471+Roddy192@users.noreply.github.com>
2025-05-31 19:22:24 -0300

    commit 2 Rodrigo

--- ecommerce-RM/src/components/Header.vue
+++ ecommerce-RM/src/components/Header.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        <h1>Produtos Premium</h1>
-        <p>Descubra sobre nossos produtos refinados e exclusivos </p>
+    <div class="block py-16 bg-gradient-to-r from-slate-900 via-slate800 to-slate-900 text-white">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            
+            <h1 class="text-5x1 font bols">Produtos Premium</h1>
+
+            <p>Descubra sobre nossos produtos refinados e exclusivos </p>
+        </div>
         <div class="">
             <button>All</button>
             <button>Wearables</button>
--- ecommerce-RM/vite.config.js
+++ ecommerce-RM/vite.config.js
@@ -7,5 +7,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
-  ]
-})
+  ],
+  server: {
+    port: 3000
+  }
+});
