c1fa1a1319a8e41a74b6daad1fa8e8c910f1ee6c
ma-pedroza <matheusgomeszpedroza@gmail.com>
2025-05-31 22:03:57 -0300

    Atualizando cor do header

--- ecommerce-RM/src/components/Header.vue
+++ ecommerce-RM/src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="block py-16 bg-gradient-to-r from-slate-900 via-slate800 to-slate-900 text-white">
+    <div class="block py-16 bg-gradient-to-r from-slate-900 via-slate-800 to-slate-900 text-white">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="text-center mb-12">
                 <h1 class="text-5xl font-bold mb-4 bg-gradient-to-r from-emerald-400 via-teal-400 to-cyan-400 bg-clip-text text-transparent">Premium Products</h1>
